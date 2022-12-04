d56283c0e11c465fbd522b43755d8ce2a3e5f238
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-12-04 20:16:38 +0100

    feat: BJ

---
+++ js/script.js
@@ -0,0 +1,154 @@
+window.onload = function () {
+    document
+        .getElementById("modalFDMOpen")
+        .addEventListener("click", showModalFDM);
+    document
+        .getElementById("modalFDMClose1")
+        .addEventListener("click", closeModalFDM);
+    document
+        .getElementById("modalFDMClose2")
+        .addEventListener("click", closeModalFDM);
+
+    document
+        .getElementById("modalSLAOpen")
+        .addEventListener("click", showModalSLA);
+    document
+        .getElementById("modalSLAClose1")
+        .addEventListener("click", closeModalSLA);
+    document
+        .getElementById("modalSLAClose2")
+        .addEventListener("click", closeModalSLA);
+
+    document
+        .getElementById("modalDLPOpen")
+        .addEventListener("click", showModalDLP);
+    document
+        .getElementById("modalDLPClose1")
+        .addEventListener("click", closeModalDLP);
+    document
+        .getElementById("modalDLPClose2")
+        .addEventListener("click", closeModalDLP);
+
+    document
+        .getElementById("modalMSLAOpen")
+        .addEventListener("click", showModalMSLA);
+    document
+        .getElementById("modalMSLAClose1")
+        .addEventListener("click", closeModalMSLA);
+    document
+        .getElementById("modalMSLAClose2")
+        .addEventListener("click", closeModalMSLA);
+
+    document
+        .getElementById("modalSLSOpen")
+        .addEventListener("click", showModalSLS);
+    document
+        .getElementById("modalSLSClose1")
+        .addEventListener("click", closeModalSLS);
+    document
+        .getElementById("modalSLSClose2")
+        .addEventListener("click", closeModalSLS);
+
+    document
+        .getElementById("modalSLMOpen")
+        .addEventListener("click", showModalSLM);
+    document
+        .getElementById("modalSLMClose1")
+        .addEventListener("click", closeModalSLM);
+    document
+        .getElementById("modalSLMClose2")
+        .addEventListener("click", closeModalSLM);
+
+    document
+        .getElementById("modalDMLSOpen")
+        .addEventListener("click", showModalDMLS);
+    document
+        .getElementById("modalDMLSClose1")
+        .addEventListener("click", closeModalDMLS);
+    document
+        .getElementById("modalDMLSClose2")
+        .addEventListener("click", closeModalDMLS);
+
+    document
+        .getElementById("modalEBMOpen")
+        .addEventListener("click", showModalEBM);
+    document
+        .getElementById("modalEBMClose1")
+        .addEventListener("click", closeModalEBM);
+    document
+        .getElementById("modalEBMClose2")
+        .addEventListener("click", closeModalEBM);
+
+        document
+        .getElementById("modalBJOpen")
+        .addEventListener("click", showModalBJ);
+    document
+        .getElementById("modalBJClose1")
+        .addEventListener("click", closeModalBJ);
+    document
+        .getElementById("modalBJClose2")
+        .addEventListener("click", closeModalBJ);
+};
+
+function showModalFDM() {
+    document.getElementById("modalFDM").showModal();
+}
+function closeModalFDM() {
+    document.getElementById("modalFDM").close();
+}
+
+function showModalSLA() {
+    document.getElementById("modalSLA").showModal();
+}
+function closeModalSLA() {
+    document.getElementById("modalSLA").close();
+}
+
+function showModalDLP() {
+    document.getElementById("modalDLP").showModal();
+}
+function closeModalDLP() {
+    document.getElementById("modalDLP").close();
+}
+
+function showModalMSLA() {
+    document.getElementById("modalMSLA").showModal();
+}
+function closeModalMSLA() {
+    document.getElementById("modalMSLA").close();
+}
+
+function showModalSLS() {
+    document.getElementById("modalSLS").showModal();
+}
+function closeModalSLS() {
+    document.getElementById("modalSLS").close();
+}
+
+function showModalSLM() {
+    document.getElementById("modalSLM").showModal();
+}
+function closeModalSLM() {
+    document.getElementById("modalSLM").close();
+}
+
+function showModalDMLS() {
+    document.getElementById("modalDMLS").showModal();
+}
+function closeModalDMLS() {
+    document.getElementById("modalDMLS").close();
+}
+
+function showModalEBM() {
+    document.getElementById("modalEBM").showModal();
+}
+function closeModalEBM() {
+    document.getElementById("modalEBM").close();
+}
+
+function showModalBJ() {
+    document.getElementById("modalBJ").showModal();
+}
+function closeModalBJ() {
+    document.getElementById("modalBJ").close();
+}
